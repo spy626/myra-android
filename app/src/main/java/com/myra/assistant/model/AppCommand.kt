f99e9f78fa330e5a25472b450aea1d4579f5a4c2
spy626 <everspy626@gmail.com>
2026-08-22 02:16:28 +0530

    Add natural YouTube play intent

--- app/src/main/java/com/myra/assistant/model/AppCommand.kt
+++ app/src/main/java/com/myra/assistant/model/AppCommand.kt
@@ -4,6 +4,7 @@ sealed interface AppCommand {
     data class OpenApp(val appName: String) : AppCommand
     data class CloseCurrentApp(val requestedName: String? = null) : AppCommand
     data class SearchYouTube(val query: String) : AppCommand
+    data class PlayYouTube(val query: String?) : AppCommand
     data object RepeatYouTubeSearch : AppCommand
     data class DeepResearch(val query: String?) : AppCommand
     data class ReplyWhatsApp(val sender: String?, val message: String) : AppCommand
