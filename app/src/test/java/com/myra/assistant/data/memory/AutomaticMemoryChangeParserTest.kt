7c92f872d6f2680e8854b8bfe888a5096add7da3
spy626 <everspy626@gmail.com>
2026-08-24 09:38:08 +0530

    Test preference correction and forgetting

---
+++ app/src/test/java/com/myra/assistant/data/memory/AutomaticMemoryChangeParserTest.kt
@@ -0,0 +1,45 @@
+package com.myra.assistant.data.memory
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class AutomaticMemoryChangeParserTest {
+    @Test fun ordinaryPreferenceProducesSave() {
+        val change = AutomaticMemoryChangeParser.parse("I love horror movies")
+        assertTrue(change is AutomaticMemoryChange.Save)
+        assertEquals(
+            "preference:likes:horror movies",
+            (change as AutomaticMemoryChange.Save).candidate.stableKey
+        )
+    }
+
+    @Test fun explicitNegativePreferenceRemovesMatchingMemory() {
+        listOf(
+            "I don't like horror movies anymore",
+            "I no longer like horror movies",
+            "mujhe horror movies ab pasand nahi hai"
+        ).forEach { phrase ->
+            assertEquals(
+                phrase,
+                "preference:likes:horror movies",
+                (AutomaticMemoryChangeParser.parse(phrase) as AutomaticMemoryChange.Forget).stableKey
+            )
+        }
+    }
+
+    @Test fun correctionUpdatesReplaceableFavoriteKey() {
+        val change = AutomaticMemoryChangeParser.parse(
+            "Actually, my favorite game is Minecraft"
+        ) as AutomaticMemoryChange.Save
+        assertEquals("preference:favorite:game", change.candidate.stableKey)
+        assertEquals("Zopy's favorite game is Minecraft", change.candidate.fact)
+    }
+
+    @Test fun vagueAndPersonalNegativesDoNotChangeMemory() {
+        assertNull(AutomaticMemoryChangeParser.parse("I don't like that anymore"))
+        assertNull(AutomaticMemoryChangeParser.parse("mujhe meri dost ab pasand nahi hai"))
+        assertNull(AutomaticMemoryChangeParser.parse("maybe I do not like horror movies"))
+    }
+}
