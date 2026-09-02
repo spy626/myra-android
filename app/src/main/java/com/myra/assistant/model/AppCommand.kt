50afc5e71003b59640eeb3e21a8bd3ac0e773fd6
spy626 <everspy626@gmail.com>
2026-09-03 01:56:22 +0530

    Fix foreground command model formatting

--- app/src/main/java/com/myra/assistant/model/AppCommand.kt
+++ app/src/main/java/com/myra/assistant/model/AppCommand.kt
@@ -20,7 +20,10 @@ sealed interface AppCommand {
     data object ListFeatures : AppCommand
     data class SetFlashlight(val enabled: Boolean) : AppCommand
     data class ControlMedia(val action: MediaAction) : AppCommand
-    data class ScrollYouTube(\n        val direction: ScrollDirection?,\n        val explicitlyRequestedApp: String? = null\n    ) : AppCommand
+    data class ScrollYouTube(
+        val direction: ScrollDirection?,
+        val explicitlyRequestedApp: String? = null
+    ) : AppCommand
 
     enum class ScrollDirection { DOWN, UP }
     enum class MediaAction { PAUSE, PLAY, NEXT, PREVIOUS, FIRST }
