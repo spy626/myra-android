59938e208b9277eb1d538c5704788debafe07bcf
spy626 <everspy626@gmail.com>
2026-09-12 20:17:16 +0530

    Complete AIRI Plast-Mem Android cutover

--- app/src/main/java/com/myra/assistant/MyApplication.kt
+++ app/src/main/java/com/myra/assistant/MyApplication.kt
@@ -2,8 +2,6 @@ package com.myra.assistant
 
 import android.app.Application
 import com.myra.assistant.core.AssistantController
-import com.myra.assistant.data.memory.JarvisLegacyImporter
-import com.myra.assistant.data.memory.JarvisSimpleMemoryRuntime
 import com.myra.assistant.diagnostics.VoicePipelineLogger
 
 class MyApplication : Application() {
@@ -12,7 +10,5 @@ class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         VoicePipelineLogger.initialize(this)
-        JarvisSimpleMemoryRuntime.initialize(this)
-        JarvisLegacyImporter.importOnce(this)
     }
 }
